4538e1340fa94e446fd67f6a58a807cd7c768d4f
Ildar Nurmukhametov <neodiz@gmail.com>
2023-10-25 14:39:08 +0300

    update file

--- scripts/create_mnemonic.sh
+++ scripts/create_mnemonic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -rf files/validator_keys
-docker run -it --rm -v $(pwd)/files/validator_keys:/app/validator_keys repo.mind-dev.com/sanchain/staking-deposit new-mnemonic  --num_validators=$NUM_VALIDATORS  --folder=/app --devnet_chain_setting='{"network_name":"sanchain-pos-dev","genesis_fork_version":"0x20000089","genesis_validator_root": "0x6d87d2c51a33c3b0158d9e0670a24657c95218c7de7b5e5183723dfbb1213c40"}' --eth1_withdrawal_address=$REWARD_RECIPIENT_ADDRESS
+docker run -it --rm -v $(pwd)/files/validator_keys:/app/validator_keys repo.mind-dev.com/sanchain/staking-deposit new-mnemonic  --num_validators=$NUM_VALIDATORS  --folder=/app --devnet_chain_setting='{"network_name":"sanchain-pos-dev","genesis_fork_version":"0x20000089","genesis_validator_root": "0xe770542f848ac24b8d9b38da6218cce20dc75f03f253d75c0f449fce2a2f9a4b"}' --eth1_withdrawal_address=$REWARD_RECIPIENT_ADDRESS
